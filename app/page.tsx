0edc1e9d4aca7f29c81fd1018b79465d6271213b
Kubra709 <dev@aow.co.in>
2025-07-28 10:00:24 +0530

    fixed error

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,6 @@ import { MoreVertical, File, Globe } from "lucide-react";
 import RecentSold from "./components/MainTable";
 import SalesChart from "./components/SalesChart";
 import TopProducts from "./components/TopProductCard";
-<<<<<<< HEAD
-import MainCard from "./components/MainCards";
-
-
-const performanceCards = [
-  {
-    title: 'Total Profit',
-    value: '₹3140.74',
-    icon: '/icons/bag.png',
-    iconBg: 'bg-blue-600',
-=======
 import Header from "./components/Header";
 
 const performanceCards = [
@@ -24,7 +13,6 @@ const performanceCards = [
     value: "₹3,140.74",
     icon: "/icons/bag.png",
     iconBg: "bg-blue-600",
->>>>>>> 4307a6d261a8b1acc308495d8ac102788caabdd6
   },
   {
     title: "Orders Per Day",
@@ -124,58 +112,15 @@ export default function Home() {
                 </div>
               </div>
 
-<<<<<<< HEAD
-          {/* Right - Icons and User */}
-          <div className="flex items-center gap-3">
-            {/* Settings button */}
-            <button className=" text-white p-2 rounded-full transition">
-              <Settings size={25} />
-            </button>
-
-            {/* Notification button */}
-            <button className=" text-white p-2 rounded-full transition">
-              <Bell size={25} />
-            </button>
-
-            {/* User Profile */}
-            <div className="flex items-center gap-2">
-              <Image
-                src="/admin-logo.jpg" // Replace with actual image path
-                alt="User"
-                width={25}
-                height={30}
-                className="rounded-full"
-              />
-              <div className="text-white text-sm hidden sm:block">
-                <p className="font-medium leading-tight">Ashray</p>
-                <p className="text-xs text-gray-300">ashray@company.com</p>
-=======
               {/* Right Section: Customer Coverage */}
               <div className="w-full lg:max-w-sm">
                 <div className="bg-secondary rounded-xl p-5 w-full h-full">
                   <CustomerCoverageCard />
                 </div>
->>>>>>> 4307a6d261a8b1acc308495d8ac102788caabdd6
               </div>
             </div>
           </div>
 
-<<<<<<< HEAD
-        {/* Stats Cards */}
-         <MainCard />
-
-
-        {/* Charts and Top Products */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 md:gap-6 mb-8 mt-4">
-          {/* Sales Chart */}
-          <div className="lg:col-span-2 bg-secondary rounded-xl p-3 md:p-4 w-full h-full">
-            <SalesChart />
-          </div>
-
-
-          {/* Top Products */}
-          <div className="bg-secondary rounded-xl p-4 md:p-6 mt-4 lg:mt-0 w-full max-w-[960px] mx-auto">
-=======
           {/* Charts and Top Products */}
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 md:gap-6 mb-8 mt-4 px-5 md:px-6">
             {/* Sales Chart */}
@@ -185,7 +130,6 @@ export default function Home() {
 
             {/* Top Products */}
             <div className="bg-secondary rounded-xl p-4 md:p-6 mt-4 lg:mt-0 w-full max-w-[960px] mx-auto">
->>>>>>> 4307a6d261a8b1acc308495d8ac102788caabdd6
               <TopProducts />
             </div>
           </div>
