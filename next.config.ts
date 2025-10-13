d0e16485748d42e2146f11ca524975f51a271d0b
jasschoudhary <jasschoudhary777@gmail.com>
2025-10-13 09:55:37 +0530

    created dev br

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   basePath: '/dashboard',
+ 
   eslint: {
     ignoreDuringBuilds: true,
   },
